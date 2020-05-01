0d20421990cb670ae5bc043f619c17a23b709b47
Sirus <5435333+Sirush@users.noreply.github.com>
2020-05-01 21:30:41 +0200

    First commit

---
+++ AniListBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AniListBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
